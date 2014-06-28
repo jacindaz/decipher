8bc3ebbbf396560cf66a6062fdc7a1ad8a79a875
Jacinda Zhong <jacindaz@gmail.com>
2014-06-28 12:20:17 -0400

    Adds seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Phrase.create(slang: "Push that shit",
+              description: "A must-shout phrase whenever pushing to github",
+              example: "Did you create the models? Sweet! Push that shit!",
+              upvotes: 0, downvotes: 0)
+
+
+Vote.create(phrase_id: 1, vote: 1)
